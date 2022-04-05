797bf1a502c1959a22544c57a4d88e375a6bc2b0
Manuel A. Vázquez <manuavazquez@gmail.com>
2022-04-05 19:08:56 +0200

    - 20_bundle.ipynb     - read_zip         - more parameters

--- dlsproc/bundle.py
+++ dlsproc/bundle.py
@@ -6,10 +6,14 @@ __all__ = ['read_zip']
 import pathlib
 import zipfile
 
+import pandas as pd
+
 import dlsproc.xml
 
 # Cell
-def read_zip(input_file: str | pathlib.Path):
+def read_zip(
+    input_file: str | pathlib.Path, concatenate: bool = False, return_filenames: bool = False
+) -> list | pd.DataFrame | tuple[list, list] | tuple[pd.DataFrame, list]:
     """
     Reads and parses an XML file into a `pd.DataFrame`.
 
@@ -18,6 +22,14 @@ def read_zip(input_file: str | pathlib.Path):
     - input_file: str or Path
 
         Input file.
+
+    - concatenate: bool
+
+        Whether or not to concatenate all the files in a single `pd.DataFrame`.
+
+    - return_filenames: bool
+
+        Whether or not names of the files (read) within the zip are returned.
 
     **Returns**
 
@@ -32,8 +44,11 @@ def read_zip(input_file: str | pathlib.Path):
     # zip file is opened
     with zipfile.ZipFile(input_file) as zip_file:
 
+        # for the sake of convenience
+        filenames = zip_file.namelist()
+
         # every file within it...
-        for name in zip_file.namelist():
+        for name in filenames:
 
             # ...is opened...
             with zip_file.open(name) as f:
@@ -41,4 +56,15 @@ def read_zip(input_file: str | pathlib.Path):
                 # ...and processed
                 dfs.append(dlsproc.xml.to_curated_df(f))
 
-    return dfs
+
+    if concatenate:
+
+        dfs = pd.concat(dfs, keys=filenames, names=['file name', 'entry'])
+
+    if return_filenames:
+
+        return dfs, filenames
+
+    else:
+
+        return dfs
